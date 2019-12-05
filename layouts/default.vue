39154d60c13b1bd4a3a37372e35440ba964a0a12
Costantin Bignoli <cosbgn@gmail.com>
2019-12-05 17:39:02 +0200

    added live chat

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,14 +12,26 @@
 		</div>
 	</div>
     <nuxt />
+	<div v-if="showScripts">
+		<!-- start Gist JS code-->
+		<script>    (function(d,h,w){var gist=w.gist=w.gist||[];gist.methods=['trackPageView','identify','track','setAppId'];gist.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);gist.push(e);return gist;}};for(var i=0;i<gist.methods.length;i++){var c=gist.methods[i];gist[c]=gist.factory(c)}s=d.createElement('script'),s.src="https://widget.getgist.com",s.async=!0,e=d.getElementsByTagName(h)[0],e.appendChild(s),s.addEventListener('load',function(e){},!1),gist.setAppId("rourth3t"),gist.trackPageView()})(document,'head',window);
+		</script>
+		<!-- end Gist JS code-->
+	</div>
   </div>
 </template>
 <script>
 export default {
+	data(){
+		return {showScripts:false}
+	},
 	head() {
 		return {
 			link: [ { rel: 'canonical', href: `https://jump.sh${this.$route.path && this.$route.path.endsWith("/") ? this.$route.path.slice(0, -1) : this.$route.path}` } ]
 		}
+	},
+	mounted(){
+		this.showScripts = true
 	}
 }
 </script>
