63351d24516122b209764ad4c7b8e67331f7f90b
Costantin Bignoli <cosbgn@gmail.com>
2021-02-09 11:35:06 +0100

    fixed search

---
+++ pages/search.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class='p-10'>Redirecting..</div>
+</template>
+<script>
+export default {
+	mounted(){
+		// CUSTOM REDIRECT BECAUSE LINKS ARE WRONG ON THE APPSUMO OFFER PAGE
+		let url = "/app"
+		try {
+			const urlParams = new URLSearchParams(window.location.search);
+			const term = urlParams.get('q')
+			url = url += (term ? `?search=${term}` : '')
+		}
+		catch(e){
+			// Most likely URLSearchParams is not supported
+		}
+		url = url.replace('watchesac', 'watches')
+		this.$router.push(url)
+	}
+}
+</script>
